7d7525ab6b69fe1187d235292fa56ab850552085
Evan Brown <evanbrown@google.com>
2015-06-18 09:13:03 -0700

    Print linkedlist correctly

--- ds/linkedlist.go
+++ ds/linkedlist.go
@@ -23,7 +23,10 @@ type LinkedList struct {
 func (l *LinkedList) String() string {
 	var s string
 	for ; l != nil; l = l.next {
-		s += fmt.Sprintf("%v-", l.data)
+		s += fmt.Sprintf("%v", l.data)
+		if l.next != nil {
+			s += fmt.Sprintf("-")
+		}
 	}
 	return s
 }
